c78a565ac08bb8df7176fc2ed5a20586713e3677
ArifinEko <aeko86869@gmail.com>
2024-11-20 19:36:37 +0700

    add: complete part advance function

---
+++ scripts/12-todo-list.js
@@ -0,0 +1,44 @@
+const input = document.querySelector('.js-input');
+const addButton = document.querySelector('.js-button');
+const todoEl = document.querySelector('.js-todo-list')
+const dateInput = document.querySelector('.js-input-date')
+
+const todoList = [{
+    name: 'wash dishes',
+    dueDate: '2022-12-22'
+}]
+
+renderTodoList()
+
+addButton.addEventListener('click', () => {
+    const name = input.value;
+    todoList.push(
+        {
+            name,
+            dueDate: dateInput.value
+        }
+    )
+    console.log(todoList);
+    input.value = "";
+
+    renderTodoList()
+})
+
+
+function renderTodoList() {
+
+    todoEl.innerHTML = ""
+    let todoListHTML = ''
+
+    todoList.forEach((todoObject, index) => {
+        const { name, dueDate } = todoObject
+        todoListHTML = `
+        <p>${name}</p>
+        <p>${dueDate}</p>
+        <button class="remove-button" onclick="
+            todoList.splice(${index}, 1);
+            renderTodoList()
+        ">Delete</button>`
+        todoEl.innerHTML += todoListHTML;
+    })
+}
